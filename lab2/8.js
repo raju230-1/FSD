e620205545e09f22a4bd86a3fc12f9e36673e72f
Raju <bashaboinaraju2301@gmail.com>
2025-01-31 15:18:46 +0530

    Last Commit

---
+++ lab2/8.js
@@ -0,0 +1,9 @@
+const colors = ['red', 'green', 'blue', 'yellow'];
+const [first, second] = colors;
+console.log(first);  
+console.log(second);
+
+const [ second1, , fourth] = colors;
+
+console.log(second); 
+console.log(fourth); 
