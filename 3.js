1efea83547f1c6b0320910c3d7d1b31fd9e8e400
raju230-1 <bashaboinaraju2301@gmail.com>
2025-01-31 15:28:37 +0530

    Delete 3.js

--- 3.js
+++
@@ -1,8 +0,0 @@
-function updateEmployeeDetails(employee, newRole) {
-    return { ...employee, role: newRole };
-  }
-  const employee = { name: 'Afroz', role: 'Developer', age: 28, location: 'IN' };
-
-const updatedEmployee = updateEmployeeDetails(employee, 'Senior Developer');
-
-console.log(updatedEmployee);
